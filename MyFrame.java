e6e5b2eca6844ba60fa56302363f6617a5006664
Sandesh Roka <rokasandesh75@gmail.com>
2025-12-26 19:46:53 +0545

    First Frame

--- MyFrame.java
+++ MyFrame.java
@@ -1,9 +1,25 @@
 import java.awt.*;
 class Demo extends Frame {
+
+    Panel pmiddle,pmid;
     public Demo(){
         setTitle("Hello");
         setSize(700,500);
         setBackground(Color.BLACK);
+        setLayout(new GridBagLayout());
+
+        pmiddle = new Panel();
+        pmiddle.setLayout(new GridBagLayout());
+        pmiddle.setBackground(Color.white);
+        pmiddle.setPreferredSize(new Dimension(70,70));
+        add(pmiddle);
+
+        pmid = new Panel();
+        pmid.setBackground(Color.red);
+        pmiddle.add(pmid);
+
+
+
         setVisible(true);
     }
 }
