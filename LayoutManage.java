9c2c0dc8f338327fbb299a18d8e9a5f95b420daf
Sandesh Roka <rokasandesh75@gmail.com>
2025-12-26 19:25:54 +0545

    Updated color only

--- LayoutManage.java
+++ LayoutManage.java
@@ -2,6 +2,11 @@ import java.awt.*;
 class Layout extends Frame{
 
     Panel ptop,upper,middle,btm;
+    MenuBar mb;
+    Menu mFile,mEdit,mColor;
+    MenuItem miNew,miClose,miSeperate1;
+    MenuItem miUndo,miSeperate2,miCopy,miSeperate3,miPaste;
+    MenuItem miRed,miSeperate4,miGreen;
 
     public Layout()
     {
@@ -10,8 +15,40 @@ class Layout extends Frame{
         setBackground(Color.red);
         setLayout(null);
         setResizable(false);
-        
 
+        mb = new MenuBar();
+
+        mFile = new Menu("File");
+        mEdit = new Menu("Edit");
+        mColor = new Menu("Color");
+
+        mb.add(mFile);
+        mb.add(mEdit);
+        mb.add(mColor);
+
+        miNew = new MenuItem("New");
+        miSeperate1 = new MenuItem("-");
+        miClose = new MenuItem("Close");
+
+        mFile.add(miNew);
+        mFile.add(miSeperate1);
+        mFile.add(miClose);
+
+        miUndo = new MenuItem("Undo");
+        miSeperate2 = new MenuItem("-");
+        miCopy = new MenuItem("Copy");
+        miSeperate3 = new MenuItem("-");
+        miPaste = new MenuItem("Paste");
+
+        mEdit.add(miUndo);
+        mEdit.add(miSeperate2);
+        mEdit.add(miCopy);
+        mEdit.add(miSeperate3);
+        mEdit.add(miPaste);
+
+        setMenuBar(mb);
+        
+        
         ptop = new Panel();
         ptop.setLayout(null);
         ptop.setBounds(15,50,670,305);
@@ -24,6 +61,18 @@ class Layout extends Frame{
         upper.setBackground(Color.white);
         upper.setBounds(5,5,660,60);
         ptop.add(upper);
+
+        middle = new Panel();
+        middle.setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
+        middle.setBackground(Color.red);
+        middle.setBounds(5,70,660,40);
+        ptop.add(middle);
+
+        btm= new Panel();
+        btm.setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
+        btm.setBackground(Color.white);
+        btm.setBounds(5,115,660,70);
+        ptop.add(btm);
         
 
 
